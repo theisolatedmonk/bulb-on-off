b736dabe202549aee0995e5c9ca31978026e30a5
theisolatedmonk <96rahulseth@gmail.com>
2023-03-18 01:15:47 +0530

    second-commit

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const imgBlubOff = './image/bulb_off-removebg.png'
 const imgBlubOn = './image/bulb_on-removebg.png'
 
 let bulb = false
+let btn = false
 togggleImg()
 
 
 switchbtn.addEventListener('click',
     function() {
         togggleImg()
+        togggleSwitch()
+
     }
 )
 
@@ -18,5 +21,11 @@ switchbtn.addEventListener('click',
 function togggleImg() {
     bulb = !bulb
     blubImg.src = bulb ? imgBlubOn : imgBlubOff
+
+}
+
+function togggleSwitch() {
+    btn = !btn
+    switchbtn.innerHTML = btn ? "On" : "Off"
 
 }
